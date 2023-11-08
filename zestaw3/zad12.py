06c44df8d789295062f82a1a1ffe217f4fce0a7d
mistrzegiptu <mistrzegiptu@gmail.com>
2023-11-08 12:45:01 +0100

    set 3 task 12

---
+++ zestaw3/zad12.py
@@ -0,0 +1,26 @@
+from random import *
+
+N = int(input())
+t = [randrange(1, 99, 2) for _ in range(N)]
+
+longestPositive = 0
+longestNegative = 0
+
+for i in range(N-1):
+    j = i
+    length = 1
+    r = t[j+1] - t[j]
+    while j < N-1:
+        if t[j+1]-t[j] != r:
+            break
+        length += 1
+        j += 1
+    if r > 0:
+        longestPositive = max(length, longestPositive)
+    else:
+        longestNegative = max(length, longestNegative)
+    
+print(t)
+print(longestPositive)
+print(longestNegative)
+print(longestPositive-longestNegative)
---
+++ zestaw3/zad5.py
@@ -0,0 +1,7 @@
+t = [0 for _ in range(10)]
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    
